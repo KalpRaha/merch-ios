93be805044f5673e2b92be69e9bb75675da71c13
Sooraj kahar <sooraj@merchantech.com>
2025-12-31 10:52:15 +0530

    + FontFamily class added - as per the discussion with developer pallavi, i have removed the old Circular STD fonts from project since it is no longer usable

---
+++ QuickveeApp/Utils/FontFamily.swift
@@ -0,0 +1,33 @@
+//
+//  FontFamily.swift
+//  QuickveeApp
+//
+//  Created by Sooraj kahar on 31/12/25.
+//
+
+import UIKit
+
+
+enum FontFamily : String {
+    
+    case ManropeMedium = "Manrope-Medium"
+    case ManropeSemiBold = "Manrope-SemiBold"
+    case ManropeBold = "Manrope-Bold"
+    case ManropeExtraBold = "Manrope-ExtraBold"
+
+    
+    func size(_ size : CGFloat) -> UIFont {
+        UIFont(name: self.rawValue, size: size) ?? UIFont.systemFont(ofSize: size)
+    }
+    
+    static func printFonts() {
+        for family in UIFont.familyNames {
+            print("\(family)")
+
+            for name in UIFont.fontNames(forFamilyName: family) {
+                print("   \(name)")
+            }
+        }
+    }
+    
+}
