32d0af4e4c02b57368aaaffab236eda939ea5ca8
Sooraj kahar <sooraj@merchantech.com>
2026-01-06 14:53:34 +0530

    + base url constant made dynamic from xcconfig file

--- QuickveeApp/API/AppURLs.swift
+++ QuickveeApp/API/AppURLs.swift
@@ -9,11 +9,14 @@ import Foundation
 
 struct AppURLs {
     // live
-    static let base_url = "https://api-ci.quickvee.us/"
+//    static let base_url = "https://api-ci.quickvee.us/"
     
     //test
    // static let base_url = "https://qa-api-ci.quickvee.us/"
 
+    
+    static let base_url = { APIConstant.baseURL }()
+    
     
     static let ORDERS_LIST = "\(base_url)orders/order_list"
     static let ORDER_DETAILS = "\(base_url)orders/order_details"
